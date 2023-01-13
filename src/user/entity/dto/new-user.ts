8b9b2afa6f50918bb01440f1704a7e0eba766c93
thiru2403 <thiruselvam20000@gmail.com>
2023-01-13 13:43:26 +0530

    first commit

--- src/user/entity/dto/new-user.ts
+++ src/user/entity/dto/new-user.ts
@@ -1,10 +1,11 @@
-import { IsEmail, IsNotEmpty, IsNumber, IsString, Max, MaxLength, maxLength, Min, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsNumber, IsPhoneNumber, isPhoneNumber, IsString, Length, Max, MaxLength, maxLength, Min, MinLength } from "class-validator";
 
 export class NewUserDto{
     @IsString()
     name:string;
 
    @IsNumber()
+   @IsPhoneNumber()
    phoneNumber:number;
 
     @IsEmail()
